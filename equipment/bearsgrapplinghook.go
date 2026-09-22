8cef75e689045daba75f294e2d287efe6f2bf25e
AurélienH <aurelien.heng@ynov.com>
2026-09-23 00:10:54 +0200

    added equipment

---
+++ equipment/bearsgrapplinghook.go
@@ -0,0 +1,5 @@
+package jeu
+
+func NewBearHook() Equipment {
+	return Equipment{ID: "bear_hook", Name: "Grappin d'ours", Healthbonus: 50, Damagebonus: 32}
+}
---
+++ equipment/bearspropulsor.go
@@ -0,0 +1,5 @@
+package jeu
+
+func NewBearProp() Equipment {
+	return Equipment{ID: "bear_prop", Name: "Propulseur d'ours", Gaz: 100, Healthbonus: 130}
+}
