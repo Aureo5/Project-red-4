a4b158076bacf8b8b286dc75414751fb1659a278
AurélienH <aurelien.heng@ynov.com>
2026-09-22 16:53:52 +0200

    added equipment

---
+++ equipment/bearsclaw.go
@@ -0,0 +1,5 @@
+package jeu
+
+func NewBearClaw() Equipment {
+	return Equipment{ID: "bear_claw", Name: "Griffe d'ours", Durability: 100, Damagebonus: 47}
+}
---
+++ equipment/wolfsfang.go
@@ -0,0 +1,5 @@
+package jeu
+
+func NewWolfFang() Equipment {
+	return Equipment{ID: "wolf_fang", Name: "Croc de loup", Durability: 100, Damagebonus: 31}
+}
